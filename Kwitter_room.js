3394f88bec8f495e64fbc22cf8934eea4c14ad2c
Karthik <karthik09av@gmail.com>
2023-07-21 09:53:48 -0400

    Part 2

--- Kwitter_room.js
+++ Kwitter_room.js
@@ -1,7 +1,43 @@
-function addUser() {
-    username = document.getElementById("username").value;
-  
-      localStorage.setItem("username", username);
 
-      window.location = "Kwitter_room.html";
-  }
+//ADD YOUR FIREBASE LINKS HERE
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDpGMbvvxUEd1TcRWwtegqsGjQUjk2BFpY",
+    authDomain: "project-94-690a5.firebaseapp.com",
+    databaseURL: "https://project-94-690a5-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "project-94-690a5",
+    storageBucket: "project-94-690a5.appspot.com",
+    messagingSenderId: "383462332089",
+    appId: "1:383462332089:web:6c0b43b4d8e1d5f6dead68"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML = "Welcome, "+ user_name + "!";
+function addRoom() {
+    room_name = document.getElementById("room_name").value;
+    firebase.database().ref("/").child(room_name).update({
+          purpose: "adding room name"
+    });
+    localStorage.setItem("room_name", room_name);
+    window.location = "kwitter_page.html" 
+}
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+     Room_names = childKey;
+    //Start code
+row = "<div class='room_name' id="+Room_names+" onclick='redirroom(this.id)'>#" + Room_names +"</div> <hr>";
+document.getElementById("output").innerHTML +=row;
+    //End code
+    });});}
+getData();
+function redirroom(name) {
+    localStorage.setItem("room_name", name);
+    window.location = "kwitter_page.html";
+}
+
+function logout() {
+    localStorage.removeItem("user_name");
+    localStorage.removeItem("room_name");
+    window.location = "index.html"
+}
